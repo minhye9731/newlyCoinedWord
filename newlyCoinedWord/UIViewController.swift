1e0d4c042bbf64554c9166ab4eb97db629959837
minhye9731 <jenney62639731@gmail.com>
2022-07-11 02:54:22 +0900

    알림창 함수 생성

---
+++ newlyCoinedWord/UIViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController.swift
+//  newlyCoinedWord
+//
+//  Created by 강민혜 on 7/11/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    // MARK: 취소와 확인이 뜨는 UIAlertController
+    func presentAlert(title: String, message: String? = nil,
+                      isCancelActionIncluded: Bool = false,
+                      preferredStyle style: UIAlertController.Style = .alert,
+                      handler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let actionDone = UIAlertAction(title: "확인", style: .default, handler: handler)
+        alert.addAction(actionDone)
+        if isCancelActionIncluded {
+            let actionCancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            alert.addAction(actionCancel)
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
